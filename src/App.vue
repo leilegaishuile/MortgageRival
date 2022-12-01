8cf3db2ede270fb0458dd5a37e5fe14838e5e0b4
gangshuixing <18655896051@163.com>
2022-12-01 22:14:00 +0800

    修改控件

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,7 @@
   start from scratch if you know enough to be dangerous. Have fun!
 -->
     <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <img
-        src="/img/beams.jpg"
-        alt=""
-        class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
-        width="1308"
-      />
+      <img alt="" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
       <div
         class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"
       ></div>
@@ -26,7 +21,7 @@
         class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
       >
         <div class="mx-auto max-w-md">
-          <img src="/img/logo.svg" class="h-6" alt="Tailwind Play" />
+          <img class="h-6" alt="Tailwind Play" />
           <div class="divide-y divide-gray-300/50">
             <div class="space-y-6 py-8 text-base leading-7 text-gray-600">
               <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
